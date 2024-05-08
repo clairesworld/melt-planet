d0fccc0d2d9b53a69e7fc38515809351af9d983a
claire <claire.guimond@gmail.com>
2024-05-08 20:08:06 +0100

    ok

--- boundarylayer.py
+++ boundarylayer.py
@@ -1,3 +1,8 @@
 import sys
 sys.path
-sys.path.append('/home/claire/Works/exo-top/py/')
+sys.path.append('/home/claire/Works/exo-top/')
+import exotop.model_1D as bl
+
+
+
+bl.evolve.build_planet(planet_kwargs, run_kwargs, solve_ODE=True)
